9820806003448b984cbacdf21f65cbb5a4f16421
devhaute <devhaute@gmail.com>
2022-06-08 23:50:15 +0900

    init project

---
+++ AlamofireOAuth/AlamofireOAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireOAuthApp.swift
+//  AlamofireOAuth
+//
+//  Created by devhaute on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireOAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
